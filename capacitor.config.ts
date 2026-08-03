e6695a2ba06a4e8a73fe0f5871100aa4ad3da320
TeacherEvan <ewiebotha@gmail.com>
2026-08-03 17:24:45 +0700

    chore: capacitor native build config + dependency bump

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.ewaldt.teacherspet',
+  appName: "Teacher's Pet",
+  webDir: 'dist'
+};
+
+export default config;
